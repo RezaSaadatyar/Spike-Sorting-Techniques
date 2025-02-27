f9cabe1a63d3873774ab5ce053dc8240f4f0b966
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2025-02-27 21:57:40 +0330

    Add files via upload

---
+++ Code/main.m
@@ -0,0 +1,9 @@
+%% =============================================================================================
+% =============================== Spike Extraction Software ====================================
+% =============================== Presented by: Reza Saadatyar =================================
+% ====================================== 2020-2022 =============================================
+% Please Run Code Main.m, the Software will run automatically and there is no need to run the 
+% Functions folder codes
+clc;clear;close all;
+addpath(genpath(cd))
+spike_extraction_software
