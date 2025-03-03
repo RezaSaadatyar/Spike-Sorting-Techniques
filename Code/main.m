c0a137d2bb1659b3995b70ffcd5ab9717a2fd292
RezaSaadatyar <Reza.Saadatyar@outlook.com>
2025-03-03 03:31:12 +0330

    add

--- Code/main.m
+++ Code/main.m
@@ -1,8 +1,10 @@
 %% =============================================================================================
-% =============================== Spike Extraction Software ====================================
-% =============================== Presented by: Reza Saadatyar =================================
-% ====================================== 2020-2022 =============================================
-% Please Run Code Main.m, the Software will run automatically and there is no need to run the 
+% ================================= Spike Extraction Software ==================================
+% ================================ Presented by: Reza Saadatyar ================================
+% ============================== Email: Reza.Saadatyar@outlook.com =============================
+% ======================================= 2019-2020 ============================================
+
+% Please Run Code main.m, the Software will run automatically and there is no need to run the 
 % Functions folder codes
 clc;clear;close all;
 addpath(genpath(cd))
